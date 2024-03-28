b4bc3f8f1abf5386b46c152a81cc52c67c0922dd
jh <nohjuhee227@gmail.com>
2024-03-28 17:58:01 +0900

    [set] eslint & prettier 설정

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    printWidth: 80,
+    singleQuote: true,
+    semi: true,
+    arrowParens: 'avoid',
+    useTabs: false,
+    tabWidth: 2,
+    trailingComma: 'all',
+    backetSpacing: true,
+    arrowParens: 'always',
+    endOfLine: auto,
+  };
