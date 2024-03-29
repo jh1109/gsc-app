ae920b8e111944a88ffa985039e42459645a4e14
jh <nohjuhee227@gmail.com>
2024-03-29 11:20:15 +0900

    [style] css reset 적용

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import GlobalStyle from './GlobalStyle';
 
 function App() {
   return (
-    <div>Hello, GSC!
+    <>
+    <GlobalStyle/>
+    <div>
+      <h1>Hello</h1><h2>GSC!</h2>
+      <p>안녕하세요</p>
     </div>
+    </>
   );
 }
 
---
+++ src/GlobalStyle.js
@@ -0,0 +1,53 @@
+import { createGlobalStyle} from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+${reset}
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  main {
+    padding-top: 4rem;
+    width: 40rem;
+    height: 100vh;
+    background: white;
+    margin: auto;
+
+    @media (max-width: 768px) {
+      width: 100%;
+    }
+  }
+
+  ul,li {
+    list-style: none;
+  }
+
+  button, input {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    outline: none;
+    font: inherit;
+  }
+
+  input {
+    width: 100%;
+    height: 100%;
+  }
+
+  .a11yHidden {
+    overflow: hidden;
+    position: absolute !important;
+    clip: rect(0, 0, 0, 0);
+    clip-path: inset(50%);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+  }
+`;
+
+export default GlobalStyle;
