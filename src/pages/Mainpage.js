94ab499cdaf21b9d901a3df15a69889f77a9b5a6
juhee <nohjuhee227@gmail.com>
2024-03-31 05:41:19 +0900

    [style] 교육장 대관

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,11 +1,13 @@
 import MainBanner from "../components/banner/MainBanner";
 import Service from "../components/mainPage/Service";
+import Service2 from "../components/mainPage/Service2";
 
 const MainPage = () => {
     return (
         <>
             <MainBanner />
             <Service />
+            <Service2 />
         </>
     )
 };
