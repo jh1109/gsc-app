434c43c6a34db3342aefe552dfd173e0124aaae8
jh <nohjuhee227@gmail.com>
2024-04-01 16:11:06 +0900

    [style] 교육 성과 아이콘 추가

--- src/components/mainPage/Result.js
+++ src/components/mainPage/Result.js
@@ -7,10 +7,10 @@ const Result = () => {
     <div className={`${"mainContent"} ${classes.result}`}>
       <p className="bold bigfont2">숫자로 한눈에 보는 GSC 교육 성과</p>
       <div className={classes.contentArea}>
-        <ResultItem subject="교육 만족도" result="95점" />
-        <ResultItem subject="누적 교육생" result="6569명" />
-        <ResultItem subject="교육 제공 고객" result="61개사" />
-        <ResultItem subject="교육 수료율" result="94%" />
+        <ResultItem imgUrl="icon/good.png" imgAlt="교육 만족도 아이콘" subject="교육 만족도" result="95점" />
+        <ResultItem imgUrl="icon/students.png" imgAlt="누적 교육생" subject="누적 교육생" result="6569명" />
+        <ResultItem imgUrl="icon/company.png" imgAlt="교육 제공 고객" subject="교육 제공 고객" result="61개사" />
+        <ResultItem imgUrl="icon/graduration.png" imgAlt="교육 수료율" subject="교육 수료율" result="94%" />
 
       </div>
       <p>*'24년.3 글로벌 소프트웨어캠퍼스 내부 데이터 기준</p>
--- src/components/mainPage/ResultItem.js
+++ src/components/mainPage/ResultItem.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import classes from './ResultItem.module.css'
 
-const ResultItem = ({ subject, result }) => {
+const ResultItem = ({ imgUrl, imgAlt, subject, result }) => {
   return (
     <div className={classes.iconWrapper}>
-      <div className={classes.icon}>아이콘</div>
+      <img src={imgUrl} alt={imgAlt} className={classes.icon}/>
       <p className={`${"bigfont1"} ${classes.title}`}>{subject}</p>
       <p className="bigfont2 boldBlue">{result}</p>
     </div>
