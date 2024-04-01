a9b1451b1e0866bf4f052920ca65f8f78266a97d
jh <nohjuhee227@gmail.com>
2024-04-01 16:22:39 +0900

    [style] ICT교육 페이지 배너

--- src/pages/ICTedu.js
+++ src/pages/ICTedu.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ICTBanner from '../components/banner/ICTBanner';
 
 const ICTedu = () => {
     return (
-        <div>
-            맞춤형 ICT 교육 페이지 입니다.
-        </div>
+        <main>
+            <ICTBanner />
+        </main>
     );
 };
 
