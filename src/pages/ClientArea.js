e282720acad27f1cdc165933c1053aea455d56de
jh <nohjuhee227@gmail.com>
2024-04-01 15:46:02 +0900

    [style] 고객사 부분 스타일 수정

--- src/pages/ClientArea.js
+++ src/pages/ClientArea.js
@@ -4,7 +4,8 @@ import classes from './ClientArea.module.css'
 
 const ClientArea = () => {
   return (
-    <div className={classes.clientArea}>
+    <div className={classes.clientWrapper}>
+     <div className={`${"mainContent"} ${classes.clientArea}`}>
       <p className={classes.sub}>글로벌 산업을 선도하는 인재를 양성하며</p>
       <p className={classes.title}> GSC와 함께 성장하고 있는 고객</p>
       <div className={classes.clientImgArea}>
@@ -19,6 +20,7 @@ const ClientArea = () => {
         <Client client_img_url="/client/client_09.jpg" />
         <Client client_img_url="/client/client_10.jpg" />
       </div>
+    </div>
     </div>
   );
 };
