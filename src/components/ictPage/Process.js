c20b3e9f09fc40b59097ebd55ac914670032a11e
jh <nohjuhee227@gmail.com>
2024-04-02 09:46:44 +0900

    [style] ict 교육 페이지 아이콘 추가

--- src/components/ictPage/Process.js
+++ src/components/ictPage/Process.js
@@ -15,11 +15,11 @@ const Process = () => {
                 디지털 전환 교육방법을 제시합니다.    
             </p>
             <div className={classes.lists}>
-               <ProcessItem imgUrl="/" title="교육문의" content="IT관련 지식이 없어도 기업의 니즈와 목표를 통한 전담 매니저와 교육 기획"/>
-               <ProcessItem imgUrl="/" title="교육설계 및 비용 협의" content="기업의 니즈를 바탕으로 설계된 맞춤형 커리큘럼과 비용 협의"/>
-               <ProcessItem imgUrl="/" title="맞춤형 커리큘럼과 전담 강사 배정" content="맞춤형 커리큘럼 구성과 효과적인 목표 달성을 위한 전담 강사 투입"/>
-               <ProcessItem imgUrl="/" title="맞춤형 교육 운영" content="원활한 교육을 위한 준비부터 상시 모니터링을 통한 전담 매니저의 밀착 관리"/>
-               <ProcessItem imgUrl="/" title="결과 공유" content="교육 내 역량 테스트부터 만족도까지 전체 교육 결과 공유로 효과 확인"/>
+               <ProcessItem imgUrl="icon/contact.png" title="교육문의" content="IT관련 지식이 없어도 기업의 니즈와 목표를 통한 전담 매니저와 교육 기획"/>
+               <ProcessItem imgUrl="icon/consulting.png" title="교육설계 및 비용 협의" content="기업의 니즈를 바탕으로 설계된 맞춤형 커리큘럼과 비용 협의"/>
+               <ProcessItem imgUrl="icon/manager.png" title="맞춤형 커리큘럼과 전담 강사 배정" content="맞춤형 커리큘럼 구성과 효과적인 목표 달성을 위한 전담 강사 투입"/>
+               <ProcessItem imgUrl="icon/customized.png" title="맞춤형 교육 운영" content="원활한 교육을 위한 준비부터 상시 모니터링을 통한 전담 매니저의 밀착 관리"/>
+               <ProcessItem imgUrl="icon/resultshare.png" title="결과 공유" content="교육 내 역량 테스트부터 만족도까지 전체 교육 결과 공유로 효과 확인"/>
                 
             </div>
         </div>
