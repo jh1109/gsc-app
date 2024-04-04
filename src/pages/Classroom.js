fe7cbe837c1c3e117338dde4b61d981824a78344
jh <nohjuhee227@gmail.com>
2024-04-04 18:06:28 +0900

    [style] 교육장 대관 소개 페이지 중 이용료

--- src/pages/Classroom.js
+++ src/pages/Classroom.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ClassroomBanner from '../components/banner/ClassroomBanner';
 import ClassroomImageArea from '../components/classroom/ClassroomImageArea';
+import FeeInfo from '../components/classroom/FeeInfo';
 
 const Classroom = () => {
     return (
         <div>
             <ClassroomBanner />
             <ClassroomImageArea />
+            <FeeInfo />
         </div>
     );
 };
