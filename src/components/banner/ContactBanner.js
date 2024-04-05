db8b4571183823335f6028c15b7819c1c8fa3aae
jh <nohjuhee227@gmail.com>
2024-04-05 10:27:53 +0900

    [style] 교육장 대관 소개 문의하기 및 footer

--- src/components/banner/ContactBanner.js
+++ src/components/banner/ContactBanner.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from './ContactBanner.module.css'
 
-const ContactBanner = () => {
+const ContactBanner = ({title, sub1, sub2, btnUrl, btn}) => {
   return (
     <div className={classes.main_banner}>
-      <div className={classes.banner_title}>무료상담으로 교육에 대한 고민을 해결하세요!</div>
+      <div className={classes.banner_title}>{title}</div>
       <div className={classes.banner_content}>
-        <p>글로벌 첨단 산업을 선도하는 맞춤형 인재 양성 교육을 확인해보세요.</p>
-        <p>문의 내용을 잠겨주시면 담당자가 빠른 시일 내에 답변드리겠습니다.</p>
+        <p>{sub1}</p>
+        <p>{sub2}</p>
       </div>
-      <Link to="/contact">교육 문의하기</Link>
+      <Link to={btnUrl}>{btn}</Link>
     </div>
   );
 };
--- src/pages/ICTedu.js
+++ src/pages/ICTedu.js
@@ -14,7 +14,12 @@ const ICTedu = () => {
             <Process />
             <Strength />
             </div>
-            <ContactBanner />
+            <ContactBanner
+                title="무료상담으로 교육에 대한 고민을 해결하세요!"
+                sub1="글로벌 첨단 산업을 선도하는 맞춤형 인재 양성 교육을 확인해보세요."
+                sub2="문의 내용을 남겨주시면 담당자가 빠른 시일 내에 답변드리겠습니다."
+                btnUrl="/contact"
+                btn="교육 문의하기" />
         </main>
     );
 };
