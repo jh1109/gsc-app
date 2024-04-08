003198f1d78dfd0365b1ace046b6d07c5843fb28
jh <nohjuhee227@gmail.com>
2024-04-08 12:41:35 +0900

    [style] 강의실 이용료 정보 수정

--- src/components/classroom/FeeInfo.js
+++ src/components/classroom/FeeInfo.js
@@ -21,17 +21,10 @@ const FeeInfo = () => {
                 </div>
                 <div className={classes.detailWrapper}>
                     <FeeDetail title="PC 강의장" sub="1일 8시간 700,000원" subject1="TECH 1 ~ 3 강의장(3개 강의장)" comment="시간 초과 시 시간당 비용 발생">
-                        <li><GrFormSubtract />CPU : AMD Ryzen 5 3400G</li>
+                        <li><GrFormSubtract />CPU : AMD Ryzen 5 3400G(TECH 1 ~ 3)</li>
+                        <li><GrFormSubtract />CPU : AMD Ryzen 5 3400G(TECH 4 ~ 5)</li>
                         <li><GrFormSubtract />RAM : 32G (DDR4 16G PC4-21300*2)</li>
                         <li><GrFormSubtract />SSD : 240GB</li>
-                        <li><GrFormSubtract />모니터 : 27인치</li>
-                        <li><GrFormSubtract />빔 프로젝트 : CANON 3600Ansi Full HD</li>
-                        <li><GrFormSubtract />각 28석 <span>(최대 30인까지 수용 가능!)</span></li>
-                    </FeeDetail>
-                    <FeeDetail title="PC 강의장" sub="1일 8시간 700,000원" subject1="TECH 4 ~ 5 강의장(2개 강의장)" comment="시간 초과 시 시간당 비용 발생">
-                        <li><GrFormSubtract />CPU : AMD Ryzen 5 PRO 4650G</li>
-                        <li><GrFormSubtract />RAM : 32G (DDR4 16G PC4-21300*2)</li>
-                        <li><GrFormSubtract />SSD : 256GB</li>
                         <li><GrFormSubtract />모니터 : 27인치</li>
                         <li><GrFormSubtract />빔 프로젝트 : CANON 3600Ansi Full HD</li>
                         <li><GrFormSubtract />각 28석 <span>(최대 30인까지 수용 가능!)</span></li>
