5c46d3f33707d94d1ee3c0270726e0ac6fd1dc15
jh <nohjuhee227@gmail.com>
2024-04-08 13:27:51 +0900

    [style] 부트캠프 교육추천 과정 레이아웃 수정

--- src/components/bootcamp/KDT.js
+++ src/components/bootcamp/KDT.js
@@ -4,21 +4,57 @@ import { KDT_DATA } from '../../data/KDTLIST';
 import classes from './KDT.module.css';
 
 const KDT = () => {
+    const IoT = KDT_DATA.filter(data=>data.group==="사물인터넷")
+    const AI = KDT_DATA.filter(data=>data.group==="빅데이터")
+    const Cloud = KDT_DATA.filter(data=>data.group==="클라우드")
     return (
         <section className={classes.KDTWrapper}>
             <h2>K-digital Training</h2>
             <h3>GSC 추천 교육과정</h3>
-            <ul>
-                {KDT_DATA.map(data => (
-                    <li key={data.id}>
-                        <Link to={data.url} target="_blank">
-                            <img src="/" alt="교육과정 thumbnail" />
-                            <figcaption className="bold">{ data.name}</figcaption>
-                            <p>{ data.description}</p>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+            <div>
+                <div>
+                    <p className={classes.ulTitle}>사물인터넷</p>
+                    <ul>
+                        {IoT.map(data => (
+                            <li key={data.id}>
+                                <Link to={data.url} target="_blank">
+                                    <img src="/" alt="교육과정 thumbnail" />
+                                    <figcaption className="bold">{ data.name}</figcaption>
+                                    <p>{ data.description}</p>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div>
+                    <p className={classes.ulTitle}>빅데이터 + 인공지능</p>
+                    <ul>
+                        {AI.map(data => (
+                            <li key={data.id}>
+                                <Link to={data.url} target="_blank">
+                                    <img src="/" alt="교육과정 thumbnail" />
+                                    <figcaption className="bold">{ data.name}</figcaption>
+                                    <p>{ data.description}</p>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div>
+                    <p className={classes.ulTitle}>클라우드</p>
+                    <ul>
+                        {Cloud.map(data => (
+                            <li key={data.id}>
+                                <Link to={data.url} target="_blank">
+                                    <img src="/" alt="교육과정 thumbnail" />
+                                    <figcaption className="bold">{ data.name}</figcaption>
+                                    <p>{ data.description}</p>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </section>
     );
 };
