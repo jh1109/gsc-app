d5729d9bebcf1a186a55720724532c0ab04cb22f
jh <nohjuhee227@gmail.com>
2024-04-09 14:21:57 +0900

    [refac] title 있는 배너 컴포넌트화

--- src/components/banner/ICTBanner.js
+++ src/components/banner/ICTBanner.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import classes from './ICTBanner.module.css'
 
-const MainBanner = () => {
+const ICTBanner = ({heading, title, sub1, sub2, className, children}) => {
   return (
-    <div className={classes.main_banner}>
-      <div className="bigfont2 bold">맞춤형 ICT 교육</div>
-      <div className={classes.banner_title}>ICT교육의 새로운 지평을 열다!</div>
-      <div className={classes.banner_content}>
-        <p>미취업자와 재직자를 위한 ICT교육, 이제 만족도 100%을 경험하세요.</p>
-        <p>당신의 성장을 위한 최적의 교육 솔루션을 만나보세요.</p>
-
-      </div>
-      <Link to="/contact">교육 문의하기</Link>
-    </div>
+    <section className={`${classes.headingBannerWrapper} ${className ? classes[className] : ""}`}>
+      <h2>{heading}</h2>
+      <p>{title}</p>
+      <p>{sub1}</p>
+      <p>{sub2}</p>
+      {children ? children : null}
+    </section>
   );
 };
 
-export default MainBanner;
+export default ICTBanner;
--- src/pages/Cases.js
+++ src/pages/Cases.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import OnlyPBanner from '../components/banner/OnlyPBanner';
 import CasesContent from '../components/cases/CasesContent';
 import Footer from '../components/layout/Footer';
+import ICTBanner from '../components/banner/ICTBanner';
 
 const Cases = () => {
     return (
         <>
-            <OnlyPBanner
-                headline="고객사례"
-                title="글로벌소프트웨어캠퍼스의 생생한 교육현장을 확인해보세요."
-                sub="글로벌소프트웨어캠퍼스의 교육 현장의 모습을 소개합니다."
-            />
+            <ICTBanner
+                    heading="고객 사례"
+                    title="글로벌소프트웨어캠퍼스와 함께 성장한 고객 사례"
+                    sub1="글로벌 산업을 선도하는 인재를 양성하며"
+                    sub2="글로벌소프트웨어캠퍼스와 함께 성장한 고객 사례를 소개합니다."
+                    className="ictBanner"
+                >
+                </ICTBanner>
             <CasesContent />
             <Footer />
         </>
