e9e44d1bf139ffe9fe0a79191f519b7ad6dc2c8b
jh <nohjuhee227@gmail.com>
2024-04-09 15:35:16 +0900

    [chore] 부트캠프 : 오타 수정

--- src/components/bootcamp/CardGuide.js
+++ src/components/bootcamp/CardGuide.js
@@ -8,7 +8,7 @@ const CardGuide = () => {
             <div className={classes.cardGuideWrapper}>
                 <p className="bold">교육비 0원!</p>
                 <p>K-Digital Training 교육은 <span className={classes.boldYellow}>국민내일배움카드 발급이 필수</span>입니다!</p>
-                <p>아직 국민내일배움카드가 없으신가요? 글로벌소프트웨어가 알려드릴게요~  </p>
+                <p>아직 국민내일배움카드가 없으신가요? 글로벌소프트웨어캠퍼스가 알려드릴게요~  </p>
                 <div><TbArrowBigDownLines /><TbArrowBigDownLines /><TbArrowBigDownLines /></div>
                 <Link to="/" target="_blank" className="bold">국민내일배움카드 발급 가이드 바로가기</Link>
             </div>
